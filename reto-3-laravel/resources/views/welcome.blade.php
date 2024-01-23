be54dd89165129be8a890fce330366afb23a41ed
Beatriz <beatriz.moran@ikasle.egibide.org>
2024-01-23 14:13:37 +0100

    role

--- reto-3-laravel/resources/views/welcome.blade.php
+++ reto-3-laravel/resources/views/welcome.blade.php
@@ -30,6 +30,11 @@
                     @endauth
                 </div>
             @endif
+            @role("administrativo")
+            <h1>Hola</h1>
+            @else
+            <h1>adios</h1>
+            @endrole
 
             <div class="max-w-7xl mx-auto p-6 lg:p-8">
                 <div class="flex justify-center">
