df221924030a8a0d473a8d096ed8fd4f67bafcb4
JavierJamaicaCast <javiernicolasjamaica@gmail.com>
2024-01-23 14:01:56 +0100

    Roles y permisson hechos

---
+++ reto-3-laravel/database/seeders/UserSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $admin = User::create([
+            'name' => 'bea',
+            'email' => 'bea@email.com',
+            'password' => Hash::make('12345678')
+        ]);
+        $admin->assignRole('responsable');
+        $admin = User::create([
+            'name' => 'javier',
+            'email' => 'javier@email.com',
+            'password' => Hash::make('12345678')
+        ]);
+        $admin->assignRole('responsable');
+        $admin = User::create([
+            'name' => 'ander',
+            'email' => 'ander@email.com',
+            'password' => Hash::make('12345678')
+        ]);
+        $admin->assignRole('responsable');
+    }
+}
