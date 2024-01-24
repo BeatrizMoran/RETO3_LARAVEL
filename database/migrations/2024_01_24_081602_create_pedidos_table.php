114dfa3658a4aee88480bfe9730dc1b74e83a8b1
Beatriz <beatriz.moran@ikasle.egibide.org>
2024-01-24 09:59:15 +0100

    migraciones, modelos y controladores creados

---
+++ database/migrations/2024_01_24_081602_create_pedidos_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pedidos', function (Blueprint $table) {
+            $table->id();
+            $table->enum('estado', ['solicitado', 'en preparacion', 'en entrega', "entregado"]);
+            $table->unsignedBigInteger('cliente_id')->nullable();
+            $table->foreign('cliente_id')->references('id')->on('clientes');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pedidos');
+    }
+};
