25f0592f01ef9a46c4ca06268a1b9caf5a4f87db
JavierJamaicaCast <javiernicolasjamaica@gmail.com>
2024-01-26 10:18:35 +0100

    Middleware corregido

--- routes/web.php
+++ routes/web.php
@@ -37,14 +37,8 @@ Route::middleware(['role:responsable'])->group(function () {
     Route::get('/dashboard', [ProductoController::class, 'dashboard'])->name('dashboard');
     Route::get('/register', [RegisterController::class, 'dashboard'])->name('dashboard');
 
-    Route::prefix('dashboard')->name('dashboard.')->group(function () {
-        Route::get('/clientes', [ClienteController::class, 'index'])->name('clientes.index');
 
-        Route::prefix('productos')->name('productos.')->group(function () {
-            Route::get('/', [ProductoController::class, 'dashboardProductos'])->name('index');
-            Route::get('/create', [ProductoController::class, 'create'])->name('create');
-            Route::post('/store', [ProductoController::class, 'store'])->name('store');
-            Route::delete('/destroy/{producto}', [ProductoController::class, 'destroy'])->name('destroy');
-        });
-    });
+    Route::get('/clientes', [ClienteController::class, 'index'])->name('clientes.index');
+    Route::resource('/productos', ProductoController::class);
+    Route::get('/', [ProductoController::class, 'dashboardProductos'])->name('dashboard.productos');
 });
