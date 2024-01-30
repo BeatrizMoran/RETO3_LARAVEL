19186c41bc456edabb7aefd2353af3c6e4368e99
Beatriz <beatriz.moran@ikasle.egibide.org>
2024-01-30 08:27:21 +0100

    middleware

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,11 @@
 <?php
-
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\ClienteController;
 use App\Http\Controllers\ProductoController;
 use App\Http\Controllers\PedidoController;
 use App\Http\Controllers\UserController;
-
-
 use App\Http\Controllers\HomeController;
+
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
@@ -18,17 +16,13 @@ Route::get('/', function () {
 })->name('welcome');
 
 
-
 Route::post('/toggle-dark-mode', [HomeController::class, "modoClaroOscuro"])->name('toggle-dark-mode');
 
 
 Auth::routes();
 
-Route::get('/catalogo', [ProductoController::class, 'catalogo'])->name('productos.catalogo');
 
-Route::get('/dashboard/profile', function (){
-    return view("profile");
-})->name("profile");
+Route::get('/catalogo', [ProductoController::class, 'catalogo'])->name('productos.catalogo');
 
 
 Route::middleware(['auth'])->group(function () {
@@ -38,29 +32,56 @@ Route::middleware(['auth'])->group(function () {
         return view('perfil');
     })->name('perfil');
 
+
+    Route::get('/productos/dashboardProductos', [ProductoController::class, 'dashboardProductos'])->name('productos.dashboardProductos');
+    Route::get('/productos/show/{producto}', [ProductoController::class, 'show'])->name('productos.show');
+    Route::get('/dashboard/productos', [ProductoController::class, 'dashboardProductos'])->name('dashboard.productos');
+
+
+
+
+
+    Route::get('/pedidos', [PedidoController::class, 'index'])->name('pedidos.index');
+    Route::get('/pedidos/show/{pedido}', [PedidoController::class, 'show'])->name('pedidos.show');
+    Route::get('/pedidos/edit/{pedido}', [PedidoController::class, 'edit'])->name('pedidos.edit');
+    Route::put('/pedidos/update/{pedido}', [PedidoController::class, 'update'])->name('pedidos.update');
+
+
+
+
 });
 
 
 Route::middleware(['role:responsable|comercial'])->group(function () {
-    // Rutas accesibles por responsable y comercial
-
-    Route::get('/dashboard/productos', [ProductoController::class, 'dashboardProductos'])->name('dashboard.productos');
 
 
-    Route::resource('/productos', ProductoController::class);
+    Route::get('/productos/edit/{producto}', [ProductoController::class, 'edit'])->name('productos.edit');
+    Route::put('/productos/update/{producto}', [ProductoController::class, 'update'])->name('productos.update');
 
 
-    //pedidos
 
-    Route::resource('/pedidos', PedidoController::class);
 
+
+    Route::get('/pedidos/create', [PedidoController::class, 'create'])->name('pedidos.create');
+    Route::post('/pedidos/store', [PedidoController::class, 'store'])->name('pedidos.store');
+    Route::delete('/pedidos/destroy/{id}', [PedidoController::class, 'destroy'])->name('pedidos.destroy');
 });
+
+
+Route::middleware(['role:responsable|administrativo'])->group(function () {
+    Route::get('/productos/create', [ProductoController::class, 'create'])->name('productos.create');
+    Route::post('/productos/store', [ProductoController::class, 'store'])->name('productos.store');
+    Route::delete('/productos/destroy/{producto}', [ProductoController::class, 'destroy'])->name('productos.destroy');
+    Route::resource('/clientes', ClienteController::class);
+});
+
 
 Route::middleware(['role:responsable'])->group(function () {
-    Route::resource('/clientes', ClienteController::class);
     Route::resource('/usuarios', UserController::class);
-
-
 });
 
+
 Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
+
+
+
