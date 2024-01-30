bf091ec6669462a62404a34fd76e18654e528560
ander <ander.mora@ikasle.egibide.org>
2024-01-30 12:52:18 +0100

    commit martes

--- routes/api.php
+++ routes/api.php
@@ -21,5 +21,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 
 
-Route::get('/productos', [ProductoController::class, 'buscarProductos']);
-//  Route::get('/productos2', [ProductoController::class, 'productosAPI']);
+//Route::get('/productos', [ProductoController::class, 'buscarProductos']);
+Route::get('/productos', [ProductoController::class, 'productosAPI']);
