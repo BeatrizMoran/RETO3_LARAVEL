eeb140328ada28d370715a919461c19034f3f45c
JavierJamaicaCast <javiernicolasjamaica@gmail.com>
2024-02-03 01:18:49 +0100

    Cambio en card

--- resources/views/_components/productoCard.blade.php
+++ resources/views/_components/productoCard.blade.php
@@ -1,5 +1,6 @@
 <div class="card shadow mb-4">
-    <img src="{{ asset('storage/images/' . $imagen) }}" class="card-img-top" alt="Imagen">
+    <img src="{{ asset('storage/images/' . $imagen) }}" class="card-img-top"style="height: 50%; object-fit: cover;"
+        alt="Imagen">
     <div class="card-body">
         <h5 class="card-title"><strong>Nombre:</strong> {{ $nombre }}</h5>
         <p class="card-text"><strong>Precio:</strong> ${{ $precio }}</p>
