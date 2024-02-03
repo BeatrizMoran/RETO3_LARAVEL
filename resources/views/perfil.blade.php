7806bb20c4faeacf13225982157169fb90f92ca5
JavierJamaicaCast <javiernicolasjamaica@gmail.com>
2024-02-03 18:34:46 +0100

    Perfil banner

--- resources/views/perfil.blade.php
+++ resources/views/perfil.blade.php
@@ -7,7 +7,9 @@
     <div class=" row d-flex justify-content-center my-4 ">
         <!-- Primer div -->
         <div class=" col-10 rounded-4 p-3 bg-primary position-relative"
-            style="height: 230px; background: url('{{ url('fondo-perfil.jpg') }}'); background-size: cover;">
+            style="height: 230px; background: url('{{ asset('/images/banner.png') }}'); background-size: cover;
+            background-position: center center;
+            background-attachment: local;">
         </div>
         <!-- Segundo div -->
         <div class=" col-12 position-absolute rounded-4 p-3 bg-light bg-degraded card card-body mx-3 mx-md-4 mt-n6"
@@ -17,12 +19,13 @@
                     Informacion Perfil
                 </div>
                 <div class="d-flex justify-content-center align-items-center">
-                    <img src="{{ asset('storage/images/' . auth()->user()->imagen) }}" alt="foto-perfil" class="rounded-circle m-2"
-                        width="100" height="100">
+                    <img src="{{ asset('storage/images/' . auth()->user()->imagen) }}" alt="foto-perfil"
+                        class="rounded-circle m-2" width="100" height="100">
                 </div>
 
                 <div class="card-body">
-                    <form action="{{ route('perfil.update', auth()->user()) }}" method="POST" enctype="multipart/form-data">
+                    <form action="{{ route('perfil.update', auth()->user()) }}" method="POST"
+                        enctype="multipart/form-data">
                         @csrf
                         @method('PUT')
                         <!-- Nombre -->
