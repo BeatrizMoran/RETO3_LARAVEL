9724c12cc02caac6214a6476d8284496d8b5fc94
Beatriz <beatriz.moran@ikasle.egibide.org>
2024-02-06 12:55:57 +0100

    seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,9 +21,9 @@ class DatabaseSeeder extends Seeder
 
         $this->call([
 
-            RoleSeeder::class,
-            UserSeeder::class,
-            ClienteSeeder::class,
+            //RoleSeeder::class,
+            //UserSeeder::class,
+            //ClienteSeeder::class,
             ProductoSeeder::class
 
 
